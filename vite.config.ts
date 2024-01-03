4bfb3612b896d1b77c8a5f87bdb4c21bc14bf8ca
felixfavour <felixfavour0@gmail.com>
2024-01-03 10:34:36 +0100

    chore: updated manifest for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,24 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
+      includeAssets: [''],
+      manifest: {
+        name: 'Youversion Search',
+        short_name: 'YouversionSearch',
+        description: 'Youversion Search enable you to elevate your Bible study experience. Discover a new level of engagement with the scriptures by filtering past notes and bookmarks on the app',
+        theme_color: '#ffab2d',
+        icons: [
+          {
+            src: 'youversion-search-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'youversion-search-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ],
